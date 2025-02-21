4e39c9659756b030f5ade190af24eb56dd14a8b2
Sam Estep <sam@samestep.com>
2025-02-20 23:22:53 -0500

    Add component to the nightly toolchain

--- .github/wasm.py
+++ .github/wasm.py
@@ -14,7 +14,7 @@ def run(cmd: list[str]) -> None:
 
 def compile() -> str:
     run(["rustup", "install", "nightly"])
-    run(["rustup", "component", "add", "rust-src"])
+    run(["rustup", "component", "add", "rust-src", "--toolchain", "nightly"])
     run(
         [
             "cargo",
