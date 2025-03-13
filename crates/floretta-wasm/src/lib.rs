3aecc4b5a6ba2eb8cad2c6b375dff3bdfd07e077
Sam Estep <sam@samestep.com>
2025-03-13 12:25:32 -0400

    Support Rust versions as old as 1.78 (#9)

--- crates/floretta-wasm/src/lib.rs
+++ crates/floretta-wasm/src/lib.rs
@@ -1,11 +1,11 @@
 use floretta::Autodiff;
 
-#[unsafe(no_mangle)]
+#[no_mangle]
 fn forward(wasm: &[u8]) -> Result<Vec<u8>, floretta::Error> {
     Autodiff::no_validate().forward(wasm)
 }
 
-#[unsafe(no_mangle)]
+#[no_mangle]
 fn reverse(wasm: &[u8]) -> Result<Vec<u8>, floretta::Error> {
     Autodiff::no_validate().reverse(wasm)
 }
